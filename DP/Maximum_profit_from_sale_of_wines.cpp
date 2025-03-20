b75470590c6bf0641f1a420b5ec17712921739b9
Shivanshu Singh <singshivanshu2643@gmail.com>
2025-03-20 12:31:21 +0530

    Updated Maximum_profit_from_sale_of_wines.cpp with DP implementation

--- DP/Maximum_profit_from_sale_of_wines.cpp
+++ DP/Maximum_profit_from_sale_of_wines.cpp
@@ -1,23 +1,31 @@
-#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
+int dp[305][305];
+string s[305][305];
 pair<int, string> f(vector<int>&prices, int start, int end, int i) {
-    if(i==prices.size()+1) return {0, ""};
-    
+    if(start>end) return {0, ""};
+
+    if(dp[start][end]!=-1) return {dp[start][end], s[start][end]};
+
     auto front = f(prices, start+1, end, i+1);
-    int fontval = prices[start]*i +front.first;
+    int fontval = prices[start]*i + front.first;
 
     auto back = f(prices, start, end-1, i+1);
     int backval = prices[end]*i + back.first;
 
     if(fontval>backval) {
-        return {fontval, "beg " + front.second};
+        dp[start][end] = fontval;
+        s[start][end] = "beg " + front.second;
     }else{ 
-        return {backval, "end " + back.second};
+        dp[start][end] = backval;
+        s[start][end] = "end " + back.second;
     }
+
+    return {dp[start][end], s[start][end]};
 }
 int main(){
     int n; cin>>n; 
+    memset(dp, -1, sizeof(dp));
     vector<int>prices(n);
     for(int i=0; i<n; i++) cin>>prices[i];
     cout<<f(prices, 0, n-1, 1).first;
