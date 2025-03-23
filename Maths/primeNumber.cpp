4494d1facd2d1f39a6dbf38146b5902a3d26e401
Shivanshu Singh <singshivanshu2643@gmail.com>
2025-03-23 14:27:34 +0530

    Prime Number

---
+++ Maths/primeNumber.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cimath>
+#include<vector>
+using namespace std;
+int main(){
+    int n; cin>>n; 
+    if(n<=1) cout<<"Not Prime";
+    for(int i=2; i<=sqrt(n); i++) {
+        if(n%i==0) {
+            cout<<"Not Prime";
+            return 0;
+        } 
+    }
+    cout<<"Prime";
+return 0;
+}
